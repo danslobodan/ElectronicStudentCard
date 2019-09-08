e7155fc42cbda7b8fc2dbab44ca29da9ca7619d5
Slobodan Dan <danastroman@gmail.com>
2019-09-08 14:13:19 +0200

    Added getters and setters for StudentCard class

--- src/models/StudentCard.java
+++ src/models/StudentCard.java
@@ -17,5 +17,69 @@ public class StudentCard {
 	public StudentCard() {
 	}
 
+	public int getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(int cardId) {
+		this.cardId = cardId;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getCurriculum() {
+		return curriculum;
+	}
+
+	public void setCurriculum(String curriculum) {
+		this.curriculum = curriculum;
+	}
+
+	public Year getEnrollmentYear() {
+		return enrollmentYear;
+	}
+
+	public void setEnrollmentYear(Year enrollmentYear) {
+		this.enrollmentYear = enrollmentYear;
+	}
+
+	public int getCurriculumYear() {
+		return curriculumYear;
+	}
+
+	public void setCurriculumYear(int curriculumYear) {
+		this.curriculumYear = curriculumYear;
+	}
+
+	public ArrayList<Exam> getExams() {
+		return exams;
+	}
+
+	public void setExams(ArrayList<Exam> exams) {
+		this.exams = exams;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
 	
 }
