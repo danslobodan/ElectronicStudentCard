1f055f5c7c98d4d60498eae36328cd7cc2fe4b48
Slobodan Dan <danastroman@gmail.com>
2019-09-14 20:28:28 +0200

    Added static isNullOrWhitespace method for strings

---
+++ src/utilities/StringExtensions.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class StringExtensions {
+
+	private StringExtensions() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static boolean IsNullOrWhitespace(String str) {
+		return str != null &&  !str.isBlank();
+	}
+}
